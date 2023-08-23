a168dfe84a66e0d86d15dbe0dafbad792de858aa
janak <niraulajanak2019@gmail.com>
2023-08-23 22:26:50 +0545

    mi439: EVO

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,9 +88,6 @@ $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
 $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 10485760)) # 10 MB
 
-ifneq ($(WITH_GMS),true)
-BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1287651328 # 1228 MB
-endif
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/sys/dev/xiaomi_sdm439_touchscreen/enable_dt2w"
--- evolution_Mi439.mk
+++ evolution_Mi439.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common evolutionOS stuff.
+$(call inherit-product, vendor/evolution/config/common.mk)
 
 # Inherit from Mi439 device
 $(call inherit-product, device/xiaomi/Mi439/device.mk)
@@ -22,12 +22,24 @@ PRODUCT_PACKAGES += \
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Mi439
-PRODUCT_NAME := lineage_Mi439
+PRODUCT_NAME := evolution_Mi439
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SDM439
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 720
+#EVEREST_MAINTAINER := HEISENBERG
+#EVEREST_BUILD_TYPE := official
+
+#Extra Stuffs
+BUILD_WITH_GAPPS := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_BLUR :=true
+TARGER_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
