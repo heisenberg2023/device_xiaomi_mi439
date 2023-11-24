2c1b1389d1d7cbaab5221b08591d46b80d632483
Jprimero15 <jprimero15@aospa.co>
2023-11-24 19:42:49 +0800

    mi439: Reduce partitions reserved sizes
    Change-Id: I78a1ce55424a23e8cbebdaf1bd7cc8177a0e869e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,7 @@ $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
 $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 83886080)) # 80 MB
 $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 41943040)) # 40 MB
+    $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 10485760)) # 10 MB
 
 ifneq ($(WITH_GMS),true)
 BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1287651328 # 1228 MB
